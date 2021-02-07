97cad5986a077168665c33d7a99f49c414ac4628
tire <timo.reijonen@helsinki.fi>
2021-02-07 14:25:57 +0200

    Full stack

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Full-Stack-Part3
+
+[Backend for 3.10](https://phnebook-backend.herokuapp.com/api/persons)
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.json())
 app.use(cors())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body '))
+app.use(express.static('build'))
 
 let persons = [
   {
